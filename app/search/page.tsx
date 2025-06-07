cd45cd9c55ffe0fdab4d69c53c7955b1deeaf76d
unknown <pristinesine@gmail.com>
2025-06-07 06:45:08 +0530

    header and search functionality v2.5

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,5 @@
 // app/search/page.tsx
-import { PageProps } from '@/types'; // Adjust the import path if your types file is elsewhere
+//import { PageProps } from '@/types'; // Adjust the import path if your types file is elsewhere
 
 export default function SearchPage({ searchParams }: PageProps) {
   const query = searchParams?.q ?? '';
